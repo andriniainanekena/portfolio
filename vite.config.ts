f6f21b0d8907d61622d15702474b228da5ab7876
Nekena Andriniaina <andriniainanekena@gmail.com>
2026-06-28 02:45:25 +0300

    chore: setup GitHub Pages deployment for Vite project

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  base: "/portfolio-v2/",
+  plugins: [react(), tailwindcss(), tsconfigPaths()]
 });
